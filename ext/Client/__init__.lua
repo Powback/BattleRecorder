444ec7e572fd3ad3f3201222f1d7433ddaa7becc
Mats Bakken <powback@gmail.com>
2020-04-27 13:56:56 +0200

    Enhance commands

--- ext/Client/__init__.lua
+++ ext/Client/__init__.lua
@@ -1,35 +1,28 @@
-Console:Register('record', 'start recording', function(args)
-	-- Print usage instructions if we got an invalid number of arguments.
-	if #args ~= 1 then
-		return 'Usage: _BattleRecorder.record <*Start/Stop/Play/Clear*>'
+Console:Register('record', 'Start recording', function(args)
+	local localPlayer = PlayerManager:GetLocalPlayer()
+	if localPlayer == nil then
+		print("No local player")
+		return
 	end
+	if localPlayer.soldier == nil then
+		print("No soldier")
+		return
+	end
+	print("Recording")
+	NetEvents:SendLocal('BattleRecorder:StartRecording')
+end)
 
-	-- Parse and validate the arguments.
-	local state = args[1]
-	if state:lower() == "start" or state:lower() == "record" then
-		local localPlayer = PlayerManager:GetLocalPlayer()
-		if localPlayer == nil then
-			print("No local player")
-			return
-		end
-		if localPlayer.soldier == nil then
-			print("No soldier")
-			return
-		end
-		print("Recording")
-		NetEvents:SendLocal('BattleRecorder:StartRecording')
-	end
+Console:Register('stop', 'Stop recording', function(args)
+	print("Stop recording")
+	NetEvents:SendLocal('BattleRecorder:Stop')
+end)
 
-	if state:lower() == "stop" then
-		print("Stop recording")
-		NetEvents:SendLocal('BattleRecorder:Stop')
-	end
-	if state:lower() == "play" then
-		print("Playing recording")
-		NetEvents:SendLocal('BattleRecorder:Play')
-	end
-	if state:lower() == "clear" then
-		NetEvents:SendLocal('BattleRecorder:Clear')
-	end
-	return nil
+Console:Register('play', 'Play recording', function(args)
+	print("Playing recording")
+	NetEvents:SendLocal('BattleRecorder:Play')
+end)
+
+Console:Register('clear', 'Clear recording', function(args)
+	print("Playing recording")
+	NetEvents:SendLocal('BattleRecorder:Clear')
 end)
